3d3fcc0264f4d569f35f7a173f9e73fe30c8093f
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-11-06 16:19:54 +0000

    created user_book join table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_books
   resources :questions
   resources :books
   resources :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_161033) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_161924) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -47,6 +47,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_161033) do
     t.string "title"
     t.integer "participant_id"
     t.integer "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
