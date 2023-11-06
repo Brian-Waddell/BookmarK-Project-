eba5c62fa3a0b50b3c78a1e5ab85d4783d7dc268
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-11-06 16:29:32 +0000

    created assoications to session_participant table

--- app/models/session_participant.rb
+++ app/models/session_participant.rb
@@ -9,4 +9,12 @@
 #  user_id    :integer
 #
 class SessionParticipant < ApplicationRecord
+  belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
+
+  belongs_to :group, required: true, class_name: "GroupSession", foreign_key: "group_id"
+
+  has_many  :comments, class_name: "Comment", foreign_key: "participant_id", dependent: :destroy
+
+  has_many  :questions, class_name: "Question", foreign_key: "participant_id", dependent: :destroy 
+  
 end
