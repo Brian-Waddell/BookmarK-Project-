a76d2fba9e27bf70eb9a17457943118cd389299b
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-11-08 15:29:41 +0000

    fixed user sign in routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
   before_action :authenticate_user!
+
+
+  private
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,17 @@ Rails.application.routes.draw do
   resources :books
   resources :comments
   resources :group_sessions
-  resources :users
+  resources :users 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "articles#index"
+
+  authenticated :user do
+    # TODO: maybe change to "dashboard#index"
+    root "group_sessions#index", as: :authenticated_root
+  end
+  
+  unauthenticated :user do
+    root "articles#index", as: :unauthenticated_root
+  end
 end
